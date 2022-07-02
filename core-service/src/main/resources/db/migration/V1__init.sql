57949fd18806aef67a56ee234c097bd49fa52cbf
AleksandrTimonin <grAleen@ya.ru>
2022-07-02 16:19:07 +0300

    C-7-2 рефакторинг базы данных изменена таблица заявки-заказы(orders) добавлена таблица процессов исполнения(processes) добавлена таблица коммитов (commits) добавлена таблица временных отрезков (time_points)

--- core-service/src/main/resources/db/migration/V1__init.sql
+++ core-service/src/main/resources/db/migration/V1__init.sql
@@ -1,22 +1,65 @@
 create table orders
 (
     id                  bigserial primary key,
+    is_active           boolean not null,
 
     title               varchar(100) not null,
     description         varchar(2000) not null,
-    username            varchar(100) not null,
 
-    executor            varchar(100) not null,
-    executor_commit     varchar(2000) not null,
+    user_nick           varchar(25) not null,
+    user_id             bigserial not null,
+    user_short_name     varchar(25) not null,
+    user_long_name      varchar(100) not null,
 
     status              varchar(16) not null,
+    executors           varchar(500),
 
-    assignment          timestamp,
-    start_progress      timestamp,
-    executed            timestamp,
 
+    completed_at        timestamp,
     created_at          timestamp default current_timestamp,
     updated_at          timestamp default current_timestamp
+);
+create table processes
+(
+    id                  bigserial primary key,
+    order_id            bigserial not null,
+    is_active           boolean not null,
+    on_confirm          boolean not null,
+
+    executor_id         bigserial not null,
+    executor_short_name varchar(25) not null,
+    executor_long_name  varchar(100) not null,
+
+
+
+    assigned_at         timestamp default current_timestamp,
+    accepted_at         timestamp,
+    finished_at         timestamp,
+    in_process-total    timestamp,
+
+    updated_at          timestamp default current_timestamp,
+    FOREIGN KEY (order_id)  REFERENCES orders (id)
+);
+create table commits (
+        id                  bigserial primary key,
+        order_id            bigserial not null,
+        executor_commit     varchar(2000),
+        created_at          timestamp default current_timestamp,
+        updated_at          timestamp default current_timestamp,
+        FOREIGN KEY (order_id)  REFERENCES orders (id)
+
+);
+create table time_points(
+        id                  bigserial primary key,
+        process_id          bigserial not null,
+        executor_id         bigserial not null,
+        status              varchar(12) not null,
+        created_at          timestamp default current_timestamp,
+        updated_at          timestamp default current_timestamp,
+        FOREIGN KEY (process_id)  REFERENCES processes (id),
+        FOREIGN KEY (executor_id)  REFERENCES processes (executor_id)
+
+
 );
 
 
@@ -24,16 +67,21 @@ create table orders
 
 
 
-insert into orders (title, description, username, executor, executor_commit, status)
-values ('create app',
+insert into orders (is_active,title, description, user_nick,user_id,user_short_name,user_long_name, status,executors)
+values ( true,
+        'create app',
         'blabla bla uchet vremeni bla bla blalalalala',
         'admin',
-        'admin',
-        'blalbalbla',
-        'expected');
+        5,
+        'Aleksandrov.AA',
+        'Alexandrov Alexandr Alexandrovich',
+        'expected',
+        'admin');
 
-
-
+insert into processes (order_id, is_active,executor_id,executor_short_name,executor_long_name)
+values (1,true,false,5,'Aleksandrov.AA','Alexandrov Alexandr Alexandrovich');
+insert into commits (order_id, executor_commit)
+values (1,'Aleksandrov.AA >> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 
 
 

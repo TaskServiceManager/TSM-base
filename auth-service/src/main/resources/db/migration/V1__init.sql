c54de662399b66765335853d43d547afb4ae3b02
AleksandrTimonin <grAleen@ya.ru>
2022-07-06 12:38:43 +0300

    база данных auth сервиса изменена

--- auth-service/src/main/resources/db/migration/V1__init.sql
+++ auth-service/src/main/resources/db/migration/V1__init.sql
@@ -1,38 +1,38 @@
 create table users
 (
-    id         bigserial primary key,
-    username   varchar(36) unique,
-    password   varchar(80) not null,
+    id                                  bigserial primary key,
+    username                            varchar(36) unique,
+    password                            varchar(80) not null,
 
-    first_name   varchar(80) not null,
-    last_name   varchar(80) not null,
-    patronymic   varchar(80) not null,
+    first_name                          varchar(80) not null,
+    last_name                           varchar(80) not null,
+    middle_name                         varchar(80) not null,
 
-    email      varchar(50) unique,
-    company   varchar(80) not null,
-    phone   varchar(80) not null,
+    email                               varchar(50) unique,
+    company                             varchar(80) not null,
+    company_email                       varchar(50) ,
+    work_position                       varchar(50) ,
+    phone                               varchar(80) not null,
 
-    building   varchar(80) not null,
-    office   varchar(80) not null,
+    building                            varchar(80) not null,
+    office                              varchar(80) not null,
 
-    created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp
+    created_at                          timestamp default current_timestamp,
+    updated_at                          timestamp default current_timestamp
 );
 
 create table roles
 (
-    id         bigserial primary key,
-    name       varchar(50) not null,
-    created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp
+    id                                  bigserial primary key,
+    name                                varchar(50) not null
 );
 
 create table users_roles
 (
-    user_id    bigint not null references users (id),
-    role_id    bigint not null references roles (id),
-    created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp,
+    user_id                             bigint not null references users (id),
+    role_id                             bigint not null references roles (id),
+    created_at                          timestamp default current_timestamp,
+    updated_at                          timestamp default current_timestamp,
     primary key (user_id, role_id)
 );
 
@@ -45,11 +45,11 @@ values  ('ROLE_USER'),
 
 
 
-insert into users (username, password,first_name, last_name,patronymic ,email,company,phone,building,office)
-values ('user', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'Vasiliy','Vasiliev','Vasilievich','v@gmail.com','CTO','2-22-222','B','105'),
-       ('executor', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'Boris','Borisov','Borisovich','b@gmail.com','SUP','1-11-111','G','106'),
-       ('manager', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'Dmitriy','Dmitriev','Dmitrievich','d@gmail.com','SUP','3-33-333','G','105'),
-       ('admin', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'Alexandr','Alexandrov','Alexandrovich','a@gmail.com','SUP','7-77-777','G','105');
+insert into users (username, password,first_name, last_name,middle_name ,email,company,company_email,work_position,phone,building,office)
+values ('user', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'Vasiliy','Vasiliev','Vasilievich','v@gmail.com','CTO','CTO@lalla.lu','worker','2-22-222','B','105'),
+       ('executor', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'Boris','Borisov','Borisovich','b@gmail.com','SUP','SUp@kuku.ku','admin','1-11-111','G','106'),
+       ('manager', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'Dmitriy','Dmitriev','Dmitrievich','d@gmail.com','SUP','SUp@kuku.ku','manager','3-33-333','G','105'),
+       ('admin', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'Alexandr','Alexandrov','Alexandrovich','a@gmail.com','SUP','SUp@kuku.ku','super admin','7-77-777','G','105');
 
 insert into users_roles (user_id, role_id)
 values (1, 1),

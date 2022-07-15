8ea3e32ecff9e18878ed8fcb661c1ff6bd2158c1
WhiteGray <WhiteGray-Black@yandex.ru>
2022-07-15 06:10:14 +0300

    Swaggers (api, auth-sevice) 2

--- auth-service/src/main/java/com/sanjati/auth/services/UserService.java
+++ auth-service/src/main/java/com/sanjati/auth/services/UserService.java
@@ -4,6 +4,8 @@ package com.sanjati.auth.services;
 import com.sanjati.auth.entities.Role;
 import com.sanjati.auth.entities.User;
 import com.sanjati.auth.repositories.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -22,10 +24,26 @@ import java.util.stream.Collectors;
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
 
+    @Operation(
+            summary = "Поиск имени пользователя",
+            responses = {
+                    @ApiResponse(
+                            description = "Успешный ответ", responseCode = "200"
+                    )
+            }
+    )
     public Optional<User> findByUsername(String username) {
         return userRepository.findByUsername(username);
     }
 
+    @Operation(
+            summary = "Загрузка пользователя по имени",
+            responses = {
+                    @ApiResponse(
+                            description = "Успешный ответ", responseCode = "200"
+                    )
+            }
+    )
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -33,10 +51,27 @@ public class UserService implements UserDetailsService {
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
     }
 
+    @Operation(
+            summary = "Загрузка списка ролей",
+            responses = {
+                    @ApiResponse(
+                            description = "Успешный ответ", responseCode = "200"
+                    )
+            }
+    )
     private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
         return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
 
+
+    @Operation(
+            summary = "Поиск пользователя по ID",
+            responses = {
+                    @ApiResponse(
+                            description = "Успешный ответ", responseCode = "200"
+                    )
+            }
+    )
     public Optional<User> findByUserId(Long userId) {
         return userRepository.findById(userId);
     }

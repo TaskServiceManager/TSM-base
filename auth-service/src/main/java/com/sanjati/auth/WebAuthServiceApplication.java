227467f7178bacb57a5f93ebdbb2f831433cf285
AleksandrTimonin <grAleen@ya.ru>
2022-08-15 20:12:14 +0300

    G-1-1 не рабочее - не прикосаться!

--- auth-service/src/main/java/com/sanjati/auth/WebAuthServiceApplication.java
+++ auth-service/src/main/java/com/sanjati/auth/WebAuthServiceApplication.java
@@ -3,8 +3,13 @@ package com.sanjati.auth;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
 @Slf4j
+@EnableEurekaClient
 @SpringBootApplication
+
 public class WebAuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebAuthServiceApplication.class, args);
